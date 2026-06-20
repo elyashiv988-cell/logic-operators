1573e83827ad6b227386f34c3af97e0f6d0626b6
Elyashiv <elyashiv988@gmail.com>
2026-06-20 22:49:08 +0300

    first

---
+++ logic_file.py
@@ -0,0 +1,15 @@
+#namber 1
+is_online = True
+has_access = False
+print(is_online and has_access)
+print(is_online or has_access)
+# namber 3
+status=False
+print (not status)
+# namber 4
+age=20
+hsa_id=True
+print(age <= 18 and has_id==True)
+#namber 5
+level=3
+print(1< level >5)
