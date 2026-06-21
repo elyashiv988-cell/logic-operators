a8f04df9d94fa37b86b26b84234e6bc7fd5279d8
elyashiv988-cell <elyashiv988@gmail.com>
2026-06-21 14:38:27 +0300

    Create self learn.py

---
+++ self learn.py
@@ -0,0 +1,17 @@
+#understanting qeustions
+#1 in some cases it is enough  to know one unput to determine its value
+#Example: If I have a variable x and I want to check whether ten parts of x is greater than two,
+#  if I run a command, it could be that x is zero and then it will return a division by zero error.
+#  So first I will run a command: Is x != 0 and  (10/x) > 2
+#2 first and , and then or
+#3 == checks if they are equal (same value)
+# IS checks if they are the same object in memory
+a = 12
+b = 10
+print(bin(a))
+print(bin(b))
+print(a ^ b)
+print(a&b)
+print(a | b)
+print(bin(a&b))
+print(bin(a | b))
